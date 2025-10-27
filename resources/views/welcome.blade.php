5f2c0ea1223af05de79f7fe7f45b6f9e1891336f
Gashumba Aimable Pacifique <gashpaci@gmail.com>
2025-10-28 01:21:04 +0200

    Fix missing closing nav tag in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                     @endif
                 </div>
             </div>
-
+        </nav>
         
         <!-- Hero Section -->
         <section class="pt-32 pb-20 px-4 sm:px-6 lg:px-8 bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50">
